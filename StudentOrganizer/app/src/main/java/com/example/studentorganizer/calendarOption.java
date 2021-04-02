59164dd6a72f5f20157f436d64c130d99722aa3b
Paancakes <63883672+Paancakes@users.noreply.github.com>
2021-04-02 16:21:24 -0400

    Add files via upload

---
+++ StudentOrganizer/app/src/main/java/com/example/studentorganizer/calendarOption.java
@@ -0,0 +1,14 @@
+package com.example.studentorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calendarOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_option);
+    }
+}
